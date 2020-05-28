418b2ec344c51edf561e0a3226164f07a289797f
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-05-28 20:00:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,32 +1,32 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  collection.each do |item_info|
-    if item_info[:item] == name
-      return item_info
-    end
-  end
-  nil  
-end
+ counter = 0 
+ while counter < collection.length 
+ if collection[counter][:item] == name
+ return collection [counter]
+ end
+ counter += 1
+ end
+ nil
+ end
 
 def consolidate_cart(cart)
-  new_cart = []
-  cart.each do |cart_item|
-    item_name = cart_item[:item]
-    
-    item_info =
-find_item_by_name_in_collection(item_name, new_cart)
-if item_info
-  item_info[:count] += 1 
-else 
-  new_cart << {
-    :item => item_name,
-    :price => cart_item[:price],
-    :clearance => cart_item[:clearance],
-    :count => 1 
-  }
-end
-end
-new_cart
-end
-
-  
+ new_cart = [] 
+ counter = 0 
+ while counter < cart.length 
+ new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+ if new_cart_item != nil 
+   new_cart_item[:count] += 1
+ else
+   new_cart_item = {
+     :item => cart[counter][:item],
+     :price => cart[counter][:price],
+     :clearance => cart[counter][:clearance],
+     :count => 1 
+   }
+   new_cart << new_cart_item
+ end
+ counter += 1
+ end 
+ new_cart 
+ end
